8e43aaa15a979b845f0949f77c0c03889b800d97
eminbulbul <mehmeteminbul@gmail.com>
2022-02-27 12:27:27 +0300

    app js düzenlendi. map() ile filter etme eklenecek

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
-let player = 'abac'
 
-const kelime = ["abacı","abalı","abadi"];;
+//5 harfli türkçe kelimelerin olduğu array (five_letters.js den geliyor)
+const letters = fiveLetters;
 
-let letterOne = document.getElementById('letterone')
-let letterTwo = document.getElementById('letterone')
+const findWord =document.getElementById("find");
+let letterOne = document.getElementById('letterone');
+let letterTwo = document.getElementById('lettertwo');
+let letterThree = document.getElementById('letterthree');
+let letterFour = document.getElementById('letterfour');
+let letterFive = document.getElementById('letterfive');
+
+let result =document.getElementById("result");
 
 
+findWord.addEventListener("click",()=>{
+    let playerArray = [];
+  
+    if (letterOne.value != "") {
+        playerArray.push(letterOne.value);
+    }else{
+        playerArray.push("*");
+    };
+    if (letterTwo.value != "") {
+        playerArray.push(letterTwo.value);
+    }else{
+        playerArray.push("*");
+    };
+    if (letterThree.value != "") {
+        playerArray.push(letterThree.value);
+    }else{
+        playerArray.push("*");
+    };
+    if (letterFour.value != "") {
+        playerArray.push(letterFour.value);
+    }else{
+        playerArray.push("*");
+    };
+    if (letterFive.value != "") {
+        playerArray.push(letterFive.value);
+    }else{
+        playerArray.push("*");
+    };
+
+    playerArray = playerArray.join("").toLowerCase();
+
+    
+
+});
 
 
-
-for (let index = 0; index < kelime.length; index++) {
-    if (kelime[index][3] == player[3]) {
-        console.log(kelime[index]);
-    }
-}
+// for (let index = 0; index < letters.length; index++) {
+//     if (letters[index][3] == player[3]) {
+//         console.log(letters[index]);
+//     }
+// }
 
